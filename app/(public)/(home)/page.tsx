4ff2ce2152a5247005bce37586cbd37ab5455ef0
Denis <Ori21wiki@gmail.com>
2026-02-25 18:35:15 +0300

    twwet ph

--- app/(public)/(home)/page.tsx
+++ app/(public)/(home)/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Tweet } from './Tweet';
 import { TWEETS } from '../../shared/data/tweets.data';
+import { TweetForm } from './TweetForm';
 
 export default function HomePage() {
   return (
     <div>
       <h1 className='text-3xl font-bold mb-6'>Home</h1>
+      <TweetForm />
       <div className='space-y-5'>
         {TWEETS.map((tweet) => (
           <Tweet key={tweet.author} tweet={tweet} />
